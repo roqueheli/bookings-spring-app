7e9ad182917f8d927c5734b3d02ccb1c494d74e9
Roqueheli <roqueheliarenas@gmail.com>
2024-08-24 17:50:36 -0400

    extracting api-key to application.properties

--- src/main/java/com/dh/bookings_spring_app/dto/GeocodingService.java
+++ src/main/java/com/dh/bookings_spring_app/dto/GeocodingService.java
@@ -2,16 +2,18 @@ package com.dh.bookings_spring_app.dto;
 
 import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class GeocodingService {
-    private static final String GOOGLE_GEOCODE_API_URL = "https://maps.googleapis.com/maps/api/geocode/json";
-
+    private static final String GOOGLE_GEOCODE_API_URL = "https://maps.googleapis.com/maps/api/geocode/json";    
+    @Value("${google.api-key}")
+    private String apiKey;
+    
     public String getLocationFromAddress(String address) {
-        String apiKey = "AIzaSyBXK-Z8KhwRi-zhHuAOisEkF6TqyaLVImQ"; // Reemplaza con tu API Key de Google
         URI uri = URI.create(GOOGLE_GEOCODE_API_URL);
 
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUri(uri)
